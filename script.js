ae2077e65577aa01c41c0da0cc0ab7b192d39f3e
wwwyuki <144249794+wwwyuki@users.noreply.github.com>
2025-07-09 19:07:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -292,8 +292,19 @@ document.addEventListener('DOMContentLoaded', () => {
             resetButton.style.display = 'inline-block';
             return true;
         }
+    
         return false;
     }
 
     init();
 });
+// --- Service Workerの登録 ---
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(error => {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  });
+}
